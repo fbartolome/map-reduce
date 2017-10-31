3bfa7d9d13f68e43b35f04e55f6c2390f86ed687
natalia navas <natinavas@gmail.com>
2017-10-30 21:08:02 -0300

    Client using query manager

--- tp/client/src/main/java/ar/edu/itba/pod/client/Client.java
+++ tp/client/src/main/java/ar/edu/itba/pod/client/Client.java
@@ -1,23 +1,10 @@
 package ar.edu.itba.pod.client;
 
-import ar.edu.itba.pod.collators.OrderByCollator;
-import ar.edu.itba.pod.collators.TopAndOrderByCollator;
-import ar.edu.itba.pod.keyPredicates.ActivityConditionKeyPredicate;
-import ar.edu.itba.pod.keyPredicates.ProvinceKeyPredicate;
-import ar.edu.itba.pod.mappers.HomesByRegionMapper;
-import ar.edu.itba.pod.mappers.MostInhabitedProvinceDeptsMapper;
-import ar.edu.itba.pod.mappers.UnemploymentIndexByRegionMapper;
 import ar.edu.itba.pod.model.Person;
-import ar.edu.itba.pod.reducers.CountReducerFactory;
-import ar.edu.itba.pod.reducers.HomesByRegionReducerFactory;
-import ar.edu.itba.pod.reducers.UnemploymentByRegionReducerFactory;
-import ar.edu.itba.pod.utils.CLIParser;
-import ar.edu.itba.pod.utils.CSVReader;
-import ar.edu.itba.pod.utils.ConsoleArguments;
+import ar.edu.itba.pod.utils.*;
 import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.client.config.ClientConfig;
 import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.core.ICompletableFuture;
 import com.hazelcast.core.IMap;
 import com.hazelcast.mapreduce.Job;
 import com.hazelcast.mapreduce.JobTracker;
@@ -25,15 +12,16 @@ import com.hazelcast.mapreduce.KeyValueSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.*;
-import java.util.Map.Entry;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.atomic.AtomicLong;
 
-import static java.lang.System.exit;
-
 public class Client {
     private static Logger logger = LoggerFactory.getLogger(Client.class);
+
+    public static String mapName = "people";
 
     public static void main(String[] args) {
 
@@ -49,25 +37,57 @@ public class Client {
 
         final HazelcastInstance client = HazelcastClient.newHazelcastClient(ccfg);
 
-        //TODO sacar del CSV
-        IMap<Long,Person> map = client.getMap("people");
+        IMap<Long,Person> map = client.getMap(mapName);
         final AtomicLong count = new AtomicLong(0);
 
         //"./client/src/main/resources/census100.csv"
         CSVReader.readCSV(arguments.getInputPath()).stream().forEach(p -> map.put(count.getAndIncrement(), p));
 
+
+
+
         JobTracker jobTracker = client.getJobTracker("tracker");
         Job<Long,Person> job = jobTracker.newJob(KeyValueSource.fromMap(map));
+
         try {
-            ICompletableFuture<List<Entry<String,Double>>> future = job
-                    .keyPredicate(new ActivityConditionKeyPredicate("people"))
-                    .mapper(new UnemploymentIndexByRegionMapper())
-                    .reducer(new UnemploymentByRegionReducerFactory())
-                    .submit(new TopAndOrderByCollator<>(20,false,false));
-            List<Entry<String,Double>> response = future.get();
-            for(Map.Entry<String,Double> entry : response){
-                System.out.println(entry.getKey() + "\t\t" + entry.getValue());
+            PrintWriter writer = new PrintWriter(arguments.getOutPath(), "UTF-8");
+            Query query = null;
+            switch (arguments.getQueryNumber()) {
+
+                case 1:
+                    query = new QueryManager.FirstQuery();
+                    break;
+
+                case 2:
+                    query = new QueryManager.SecondQuery(arguments.getAmount(), arguments.getProvince());
+                    break;
+
+                case 3:
+                    query = new QueryManager.ThirdQuery();
+                    break;
+
+                case 4:
+                    query = new QueryManager.FourthQuery();
+                    break;
+
+                case 5:
+                    query = new QueryManager.FifthQuery();
+                    break;
+
+                case 6:
+                    query = new QueryManager.SixthQuery(arguments.getAmount());
+                    break;
+
+                case 7:
+                    //TODO
+                    System.out.println("NO ESTA IMPLEMENTADO");
+                    break;
             }
+
+            query.output(writer, query.getFuture(job).get());
+
+        }catch (IOException e) {
+            System.err.println("There was an error writing the output " + e.getMessage());
         } catch (InterruptedException e) {
             e.printStackTrace();
         } catch (ExecutionException e) {
